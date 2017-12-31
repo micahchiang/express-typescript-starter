5a076e2be91403bd14b99049b84efc42ea6fdd4d
Micah Chiang <mchiang@rentreporters.com>
2017-12-30 17:08:04 -0800

    included more dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import * as express from 'express';
+import * as bodyparser from 'body-parser';
+import * as logger from 'morgan';
 
 class App {
 
@@ -7,6 +9,7 @@ class App {
     constructor() {
         this.express = express();
         this.loadRoutes();
+        this.loadMiddleware()
     }
 
     private loadRoutes() {
@@ -15,6 +18,12 @@ class App {
             res.send('hello world');
         });
         this.express.use('/', router);
+    }
+
+    private loadMiddleware() {
+        this.express.use(bodyparser.json());
+        this.express.use(bodyparser.urlencoded({extended: true}));
+        this.express.use(logger('dev'));
     }
 }
 
